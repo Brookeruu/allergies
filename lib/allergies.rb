eaf4a5aba67147dcc5ac52fae3f83a5819ba9d83
brooke perry and Daniel Bennett <daniel@>
2018-11-15 16:13:44 -0800

    add final if statements and clean up unused code

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -6,47 +6,45 @@ class Allergies
     @allergylist = []
   end
 
-
   def allergy_finder
     allergy = Hash.new()
     allergy = { 1 => 'eggs', 2 => 'peanuts', 4 => 'shellfish', 8 => 'strawberries', 16 => 'tomatoes', 32 => 'chocolate', 64 => 'pollen', 128 => 'cats' }
 
-
     x = number
-      # until (x = 0)
-        if x >= 128
-          @allergylist.push(allergy.fetch(128))
-          x -= 128
-        end
+    if x >= 128
+      @allergylist.push(allergy.fetch(128))
+      x -= 128
+    end
+    if x >= 64
+      @allergylist.push(allergy.fetch(64))
+      x -= 64
+    end
+    if x >= 32
+      @allergylist.push(allergy.fetch(32))
+      x -= 32
+    end
+    if x >= 16
+      @allergylist.push(allergy.fetch(16))
+      x -= 16
+    end
+    if x >= 8
+      @allergylist.push(allergy.fetch(8))
+      x -= 8
+    end
+    if x >= 4
+      @allergylist.push(allergy.fetch(4))
+      x -= 4
+    end
+    if x >= 2
+      @allergylist.push(allergy.fetch(2))
+      x -= 2
+    end
+    if x >= 1
+      @allergylist.push(allergy.fetch(1))
+      x -= 1
+    end
 
-        if x >= 64
-          @allergylist.push(allergy.fetch(64))
-          x -= 64
-        end
-
-        return @allergylist
-
-      # allergy.store(1,"eggs")
-      # allergy.store(2, "peanuts")
-      # allergy.store(4, "shellfish")
-      # allergy.store(8, "strawberries")
-      # allergy.store(16, "tomatoes")
-      # allergy.store(32, "chocolate")
-      # allergy.store(64, "pollen")
-      # allergy.store(128, "cats")
-
-      # addAllergy = allergy.fetch(number)
+    return @allergylist
 
   end
-      # binding.pry
 end
-
-    #
-    # if number is greater than or = 128
-    #   subtract number from 128
-    #   if remainder is > 0
-    #     is greater than = 64
-    #
-    # if number >= 128
-    #   allergylist.push(addAllergy(128))
-    #   number = number - 128
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -3,20 +3,28 @@ require 'pry'
 require 'allergies'
 
 describe("#allergies") do
-  # it("return value for corresponding key") do
-  #   allergy1 = Allergies.new(1)
-  #   expect(allergy1.allergy_finder()).to(eq("eggs"))
-  # end
-  #
-  # it("return value for corresponding key") do
-  #   allergy1 = Allergies.new(16)
-  #   expect(allergy1.allergy_finder()).to(eq("tomatoes"))
-  # end
+  it("return value for corresponding key") do
+    allergy1 = Allergies.new(1)
+    expect(allergy1.allergy_finder()).to(eq(["eggs"]))
+  end
+
+  it("return value for corresponding key") do
+    allergy1 = Allergies.new(16)
+    expect(allergy1.allergy_finder()).to(eq(["tomatoes"]))
+  end
 
   it("return array for corresponding keys") do
     allergy1 = Allergies.new(192)
     expect(allergy1.allergy_finder()).to(eq(["cats", "pollen"]))
   end
 
+  it("return array for corresponding keys") do
+    allergy1 = Allergies.new(80)
+    expect(allergy1.allergy_finder()).to(eq(["pollen", "tomatoes"]))
+  end
 
+  it("return array for corresponding keys") do
+    allergy1 = Allergies.new(255)
+    expect(allergy1.allergy_finder()).to(eq(["cats", "pollen", "chocolate", "tomatoes", "strawberries", "shellfish", "peanuts", "eggs"]))
+  end
 end
